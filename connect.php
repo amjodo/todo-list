8b93e9f2be8fcab8c4f9f94ff994b8b554945341
Amber Douglas <douglasamber3@gmail.com>
2015-04-14 11:15:07 -0700

    Finished videos 1-4

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php 
+//Why is this important? Why does localhost have to be first?
+//This is important that it is in this order so the server, any other way no longer works
+$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+	//if it does not connect we want it to die
+	if($mysqli->connect_error){
+		die('Connect Error ('. $mysqli->connect_errno . ')'
+			. $mysqli->connect_error);
+	}
+	else{
+		echo "Connection made";
+	}
+	$mysqli->close();
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Amber's To-Do List</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="wrap">
+		
+	</div>
+
+</body>
+</html>
