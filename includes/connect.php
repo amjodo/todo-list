0d55c03c27412b266e04c902345f30ff0b8955de
Amber Douglas <douglasamber3@gmail.com>
2015-04-15 10:49:17 -0700

    Finished Milestone 5

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php 
 //Why is this important? Why does localhost have to be first?
 //This is important that it is in this order so the server, any other way no longer works
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 	//if it does not connect we want it to die
 	if($mysqli->connect_error){
 		die('Connect Error ('. $mysqli->connect_errno . ')'
